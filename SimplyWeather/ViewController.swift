1fec84f52ac2d3c51cdacbbfab4c6447e3bf2d3c
FlyingDad <Michael@michaels-imac.attlocal.net>
2017-02-13 14:50:47 -0600

    Initial Commit

---
+++ SimplyWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplyWeather
+//
+//  Created by Michael Kroth on 2/13/17.
+//  Copyright © 2017 MGK Technology Solutions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
